57ebb15921807f451644055d5e902a46012c6473
RobCam <roberto.camacho4@gmail.com>
2024-10-28 15:41:01 +0100

    Added the class Category.cs to the project. With Methods Create, Read, Updated, delete and see all Categories.

---
+++ Category.cs
@@ -0,0 +1,73 @@
+﻿public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+
+    // läser och lägger till CollectionItem-objekt i listan. OBS: CollectionItem finns inte än
+    public List<ColletionItem> Items { get; set; }
+
+    // Lista för att spara kategorier
+    private static List<Category> categories = new List<Category>();
+
+    public Category(int id, string name, string description)
+    {
+        CategoryId = id;
+        CategoryName = name;
+        Description = description;
+        CollectionsItems = new List<CollectionItem>();
+    }
+
+    // Likt CRUD
+
+    // Create
+    public static void AddCatetegory(Category category) 
+    {
+        categories.Add(category);
+    }
+
+    // Read
+    public static Category GetCatetegory(int id)
+    {
+        foreach (Category categori in categories) 
+        {
+            if (Category.CategoryId == id) 
+            {
+                return category;
+            }
+        }
+        return null;
+    }
+
+    // Upate. OBS: skall läggas till i diagrammet 
+    public static void UpdateCategory(int id, string newName, string newDescription)
+    {
+        foreach (Category category in categories) 
+        {
+            if (category.CategoryId == id) 
+            {
+                category.CategoryName = newName;
+                category.Description = newDescription;
+            }
+        }
+    }
+
+    // Delete
+    public static void RemoveCategory(int id) 
+    {
+        foreach (Category category in categories) 
+        {
+            if (category.CategoryId == id)
+            {
+                categories.Remove(category);
+                breat;
+            }
+        }
+    }
+
+    // Visa alla kategorier. OBS: skall läggas till i diagrammet 
+    public static List<Category> GetAll() 
+    {
+        return categories;
+    }
+}
