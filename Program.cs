da0ccd0bf5431ac2c556859102f67cdd715e2db6
RobCam <roberto.camacho4@gmail.com>
2024-10-29 13:58:15 +0100

    I made som tests for the Category class.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Testing VS changes!");
+﻿class Program 
+{
+    static void Main() 
+    {
+        // === Testar klassen Category ===
+        Category book = new Category(101, "Böcker", "Min boksamling");
+        Category film = new Category(202, "Filmer", "Min filmsamling");
+        Category music = new Category(303, "Musik", "Min Musiksamling");
+        Category toy = new Category(404, "Leksaker", "Min Leksaksamling");
+
+        Category.AddCatetegory(book);
+        Category.AddCatetegory(film);
+        Category.AddCatetegory(music);
+        Category.AddCatetegory(toy);
+
+        Console.WriteLine("Alla Kategorier:");
+        foreach (var category in Category.GetAll()) 
+        {
+            Console.WriteLine($"ID: {category.CategoryId}, Namn: {category.CategoryName}, Beskrivning: {category.Description}");
+        }
+        Console.ReadKey();
+    }
+}
