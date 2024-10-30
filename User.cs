7e882335a1a9acc34ff068cedc9eec48ebf87b84
RobCam <roberto.camacho4@gmail.com>
2024-10-30 11:25:11 +0100

    Added methods CRUD, GetAllUsers, LoginUser, LogoutUser

--- User.cs
+++ User.cs
@@ -15,7 +15,7 @@ namespace TheCollectorApp
 
         // lista med kollektioner
         public List<Collection> Collections { set; get; }
-        
+
         // List med användare
         private static List<User> users = new List<User>();
 
@@ -30,6 +30,79 @@ namespace TheCollectorApp
             RegistrationDate = DateTime.Now;
             IsInlogged = false;
             Collections = new List<Collection>();
+        }
+
+        // Likt CRUD
+
+        // Skapar användare
+        public static void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        // Läser användare
+        public static User GetUser(int id)
+        {
+            foreach (User user in users)
+            {
+                if (user.UserId == id)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        // Uppdaterar användare
+        public static void UpdateUser(int id, string newFirstName, string newSecondName, string newUserName, string newPassword, string newEmail)
+        {
+            foreach (User user in users)
+            {
+                if (user.UserId == id)
+                {
+                    user.FirstName = newFirstName;
+                    user.SecondName = newSecondName;
+                    user.UserName = newUserName;
+                    user.Password = newPassword;
+                    user.Email = newEmail;
+                }
+            }
+        }
+
+        // Tar bort användare
+        public static void RemoveUser(int id)
+        {
+            foreach (User user in users)
+            {
+                if (user.UserId == id)
+                {
+                    users.Remove(user);
+                    break;
+                }
+            }
+        }
+
+        // Hämtar alla användare
+        public static List<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        // Användare loggar in med användarnamn och lösenord, därefter blir inloggad.  
+        public bool LoginUser(string userName, string password)
+        {
+            if (UserName == userName && Password == password)
+            {
+                IsInlogged = true;
+                return true;
+            }
+            return false;
+        }
+
+        // Användare loggar ut
+        public void LogoutUser()
+        {
+            IsInlogged = false;
         }
     }
 }
