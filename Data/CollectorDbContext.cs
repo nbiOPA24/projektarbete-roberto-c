37ee5dc17ddd810ef5b09be0edf5d80635cf2577
RobCam <roberto.camacho4@gmail.com>
2024-11-20 15:15:42 +0100

    Added the class CollectorDbContext.cs

---
+++ Data/CollectorDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TheCollectorApp.Models;
+
+// Testar DbContext i Entity Framework Core
+
+namespace TheCollectorApp.Data
+{
+    public class CollectorDbContext : DbContext
+    {
+
+    }
+}
