fdbdacf9b64e16c780c28888e5124507738b2c78
RobCam <roberto.camacho4@gmail.com>
2024-11-21 16:37:09 +0100

    Addad DbContext and configurations to connect to database

--- Data/CollectorDbContext.cs
+++ Data/CollectorDbContext.cs
@@ -2,11 +2,37 @@ using Microsoft.EntityFrameworkCore;
 using TheCollectorApp.Models;
 
 // Testar DbContext i Entity Framework Core
+// Presentation - Enity_Framework_intro
 
 namespace TheCollectorApp.Data
 {
     public class CollectorDbContext : DbContext
     {
+        // DBset för alla klasser (objekt)
+        public DbSet<User> Users { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<CollectionItem> CollectionItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
+        // Databasoppling
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // konfigration
+        }
+
+        // Konfigurerar relationer och regler
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Gör användarnamn unik
+            modelBuilder.Entity<User>()
+            .HasIndex(u => u.UserName)
+            .IsUnique();
+
+            // Gör e-post unik
+            modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        }
     }
 }
